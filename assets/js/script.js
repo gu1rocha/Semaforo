a25011ce901a1cac2d650f54b8665ae90f248792
Guilherme Rocha <59288859+gu1rocha@users.noreply.github.com>
2022-09-13 20:37:36 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 const input = document.querySelector('input')
 
+let color = {
+  1: 'green',
+  2: 'yellow',
+  3: 'red'
+}
+
 input.addEventListener('change',()=>{
-    document.querySelector('body').classList.toggle('stop')
+  document.querySelector('body').classList.toggle('stop')
 })
+
+let atualizarAlet = () => Math.floor(Math.random() * (3) + 1);
+
+document.querySelector('.cor').classList.add(color[atualizarAlet()])
